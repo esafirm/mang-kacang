fb4a3b1c496588e92b1ddb90aa3337acd4993702
Esa Firman <esafirm21@gmail.com>
2017-12-21 14:40:54 +0700

    Add test

---
+++ src/handlers/abayhandler.test.ts
@@ -0,0 +1,35 @@
+import { willHandle, handleEvent } from './abayhandler';
+import {
+  TextMessage,
+  MessageEvent,
+  TextEventMessage,
+  EventMessage
+} from '@line/bot-sdk';
+
+const testData: Array<TextEventMessage> = [
+  textMessage('abay mau makan'),
+  textMessage('Abay mau makan'),
+  textMessage('noabay'),
+];
+
+function textMessage(message: string): TextEventMessage {
+  return {
+    id: '',
+    type: 'text',
+    text: message
+  };
+}
+
+testData.forEach(data => {
+  test(`${data.text} should be handled`, () => {
+    const event: MessageEvent = {
+      replyToken: '',
+      message: data as EventMessage,
+      type: 'message',
+      source: undefined,
+      timestamp: 1
+    };
+
+    expect(willHandle(event)).toBe(true);
+  });
+});
