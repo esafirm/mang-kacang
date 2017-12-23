1d6e5a76d55c9e16ef74b6b35a826ee638b23930
Esa Firman <esafirm21@gmail.com>
2017-12-23 17:15:49 +0700

    Add more image to abay

--- src/handlers/abayhandler.ts
+++ src/handlers/abayhandler.ts
@@ -1,3 +1,4 @@
+import * as _ from 'lodash';
 import Client from '@line/bot-sdk/dist/client';
 import { MessageEvent, TextMessage } from '@line/bot-sdk';
 import { reply } from './utils';
@@ -7,8 +8,10 @@ export function willHandle(event: MessageEvent): Boolean {
 }
 
 export function handleEvent(client: Client, event: MessageEvent): Promise<any> {
-  const abayImage =
-    'https://scontent-sit4-1.xx.fbcdn.net/v/t1.0-9/188503_1601344285846_4341498_n.jpg?oh=522807be30b698fee1f45d3349bd228c&oe=5AD05C5D';
+  const abayImage = _.shuffle([
+    'https://scontent-sit4-1.xx.fbcdn.net/v/t1.0-9/188503_1601344285846_4341498_n.jpg?oh=522807be30b698fee1f45d3349bd228c&oe=5AD05C5D',
+    'https://scontent-sit4-1.xx.fbcdn.net/v/t1.0-9/155936_121730667891072_4752079_n.jpg?oh=26f427c4358edc0d932ef238ee2c186c&oe=5AB8C090'
+  ])[0];
   return reply(client, event, {
     type: 'image',
     originalContentUrl: abayImage,
